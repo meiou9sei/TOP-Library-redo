c540a6b6670fce2f8b905c36fdef7da2257b32af
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-10-21 13:16:46 -0400

    set up empty array and function to add books to library in the future

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let myLibrary = [];
+
+// constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -8,6 +11,10 @@ function Book(title, author, pages, read) {
       read ? "read" : "not read yet" + "."
     }`;
   };
+}
+
+function addBookToLibrary() {
+  console.log("hiya");
 }
 
 let exampleBook = new Book("my awesome life", "cameron", "2000", true);
