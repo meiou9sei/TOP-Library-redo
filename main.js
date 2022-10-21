cca1c3b421893b4a270d7c697e3bd21db6cc0413
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-10-21 13:13:20 -0400

    create basic Book constructor

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello");
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${
+      read ? "read" : "not read yet" + "."
+    }`;
+  };
+}
+
+let exampleBook = new Book("my awesome life", "cameron", "2000", true);
+
+console.log(exampleBook.info());
