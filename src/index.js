4273d4c8e5eaf69e34e92eb4068d4fbf20748b23
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-29 15:10:38 -0500

    🚧 init firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import runApp from "./main";
 
+import { initializeApp } from "firebase/app";
+
 import "./normalize.css";
 import "./styles.css";
 
-console.log("hello from index.js");
+const firebaseConfig = {
+  apiKey: "AIzaSyB9XL6YZAFlGJL9XKMqgvlLgJNOMlx3YHc",
+  authDomain: "top-library-redo.firebaseapp.com",
+  projectId: "top-library-redo",
+  storageBucket: "top-library-redo.appspot.com",
+  messagingSenderId: "790264663860",
+  appId: "1:790264663860:web:c300b752614afd9f7f1f61",
+};
+
+initializeApp(firebaseConfig);
 
 runApp();
