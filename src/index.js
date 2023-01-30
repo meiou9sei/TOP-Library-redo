e04d160ff6e647286d8902de8c859f5a3cb83d0d
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-30 15:39:32 -0500

    🚧 initialize firebase app, get firestore and collection/docs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import runApp from "./main";
 
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 import "./normalize.css";
 import "./styles.css";
+
+runApp();
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9XL6YZAFlGJL9XKMqgvlLgJNOMlx3YHc",
@@ -14,6 +17,22 @@ const firebaseConfig = {
   appId: "1:790264663860:web:c300b752614afd9f7f1f61",
 };
 
+// init firebase app
 initializeApp(firebaseConfig);
 
-runApp();
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "library");
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let myFireLibrary = [];
+    snapshot.docs.forEach((doc) => {
+      myFireLibrary.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(myFireLibrary);
+  })
+  .catch((err) => console.log(err.message));
