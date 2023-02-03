dfcb3e8eb9d04a726260a4432097997d2bd5132e
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-02-03 18:32:15 -0500

    🐛 combine logiic into one, fix exponential number of books getting added by not adding new event listener every time book is added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import runApp from "./main";
-
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import { addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
 
 import "./normalize.css";
 import "./styles.css";
@@ -32,8 +31,20 @@ onSnapshot(colRef, (snapshot) => {
   });
   console.log("firebaseBooks successfully retrieved");
   console.log(myFireLibrary);
-  // run app
-  runApp(myFireLibrary, db, colRef);
+
+  // clear library
+  booksDisplay.innerHTML = "";
+  // add books back
+  myFireLibrary.forEach((book) => {
+    let newBook = new Book(
+      book.title,
+      book.author,
+      book.pages,
+      book.readStatus,
+      book.id
+    );
+    addBookToLibrary(newBook);
+  });
 });
 
 function displayWarning(text) {
@@ -51,4 +62,97 @@ function displayWarning(text) {
   const library = document.querySelector(".library");
   library.appendChild(errorDisplay);
   setTimeout(library.removeChild(displayWarning), 5000);
+}
+
+// webapp logic
+
+let myLibrary = [];
+
+// declarations
+const booksDisplay = document.getElementById("booksDisplay");
+const addBookForm = document.getElementById("addBookForm");
+addBookForm.addEventListener("submit", submitNewBook);
+
+// constructor
+function Book(title, author, pages, read, id = null) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
+}
+
+Book.prototype.info = function () {
+  return `${title} by ${author}, ${pages} pages, ${
+    read ? "read" : "not read yet" + "."
+  }`;
+};
+
+function submitNewBook(e) {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    title: addBookForm.bookTitle.value,
+    author: addBookForm.bookAuthor.value,
+    pages: addBookForm.bookPages.value,
+    readStatus: addBookForm.isBookRead.value,
+  }).then(() => {
+    console.log("adding book successful");
+    addBookForm.reset();
+  });
+}
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  addBookToDisplay(newBook);
+}
+
+function addBookToDisplay(newBook) {
+  let newBookCard = createBookCard(newBook);
+  booksDisplay.appendChild(newBookCard);
+}
+
+function createBookCard(newBook) {
+  const bookCard = document.createElement("div");
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const readStatus = document.createElement("p");
+
+  title.textContent = newBook.title;
+  author.textContent = newBook.author;
+  pages.textContent = newBook.pages;
+  readStatus.textContent = `Book read: ${newBook.read}`;
+
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(readStatus);
+
+  // remove book button
+  const discardBook = document.createElement("button");
+  discardBook.textContent = "remove book";
+  bookCard.dataset.id = newBook.id;
+  discardBook.addEventListener("click", function () {
+    console.log(bookCard.dataset.id);
+    const docRef = doc(db, "library", bookCard.dataset.id);
+    deleteDoc(docRef);
+  });
+  bookCard.appendChild(discardBook);
+
+  // toggle read button
+  const toggleRead = document.createElement("button");
+  toggleRead.textContent = "toggle read";
+  toggleRead.addEventListener("click", function () {
+    // new way
+    const bookToUpdate = doc(db, "library", bookCard.dataset.id);
+    updateDoc(bookToUpdate, { readStatus: !readStatus });
+  });
+  bookCard.appendChild(toggleRead);
+
+  // sets bookCard bg color class
+  if (newBook.read) bookCard.classList.add("bookCardRead");
+  else bookCard.classList.add("bookCardNotRead");
+
+  return bookCard;
 }
